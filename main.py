7fbccab97a55d7daba4f37cccc9d78734f5604bf
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-12-03 09:01:48 +0100

    fin

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import programacion_perceptron
 import perceptor_tensorflow_poo
 
 if __name__ == '__main__':
+
+    #el programa ejecutándolo desde aquí no da errores pero tampoco devuelve nada, si se ejecuta cada fichero, si funciona.
     programacion_perceptron.main()
     perceptor_tensorflow_poo.main()
